20a6fe587a5c2c4ac21efd486ac26bd4a06f35c6
kbchun <kbchun5712@gmail.com>
2016-09-19 19:22:38 -0700

    Create sliding dog dancer

--- src/dogDancer.js
+++ src/dogDancer.js
@@ -2,6 +2,7 @@ var makeDogDancer = function(top, left, timeBetweenSteps) {
   //var blinkyDancer = new makeDancer(top, left, timeBetweenSteps);
 
   makeDancer.call(this, top, left, timeBetweenSteps);
+  this.counter = 0;
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
@@ -21,8 +22,27 @@ makeDogDancer.prototype.step = function() {
   makeDancer.prototype.step.call(this);
 
 
-  this.$node.css({content: 'url("https://i.ytimg.com/vi/9r1yG9e0GOE/maxresdefault.jpg")',
-                  width: '300px',
-                  height: '300px'
+  this.$node.css({content: 'url("http://i.imgur.com/8YH4Z1S.gif")',
+                  width: '200px',
+                  height: '200px',
+                  border: '0'
                 });
+
+  if (this.counter % 2 === 0) {
+    this.$node.css({'-moz-transform': 'scale(-1, 1)',
+                  '-webkit-transform': 'scale(-1, 1)',
+                  '-o-transform': 'scale(-1, 1)',
+                  '-ms-transform': 'scale(-1, 1)',
+                  transform: 'scale(-1, 1)',
+                });
+    this.counter++;
+  } else {
+    this.$node.css({'-moz-transform': 'scale(1, 1)',
+                  '-webkit-transform': 'scale(1, 1)',
+                  '-o-transform': 'scale(1, 1)',
+                  '-ms-transform': 'scale(1, 1)',
+                  transform: 'scale(1, 1)'
+                });
+    this.counter++;
+  }
 };
